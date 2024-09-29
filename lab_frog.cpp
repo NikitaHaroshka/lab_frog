91a8320ef5e5234dbccc37e3b989f84eeb038516
NikitaHaroshka <nikiharoshka@gmail.com>
2024-09-29 18:50:58 +0300

    added array

--- lab_frog.cpp
+++ lab_frog.cpp
@@ -4,5 +4,12 @@ int main()
 {
     int n;
     cin >> n;
-
+    int* A = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+        if (A[i] > 1000) {
+            cerr << A[i] << " > 1000";
+            return 0;
+        }
+    }
 }
