44bf1b68e8b58affd9079519beb920eb839e8bce
NikitaHaroshka <nikiharoshka@gmail.com>
2024-10-01 17:12:38 +0300

    complete wersion

--- lab_frog.cpp
+++ lab_frog.cpp
@@ -1,87 +1,87 @@
 ﻿#include <iostream>
+
 using namespace std;
 int main()
 {
-    int n,or1=3,or2=4;
+    int n, or1 = 3, or2 = 4, j = 0;
     cin >> n;
     int* A = new int[n];
+    int* B = new int[n];
+    int* pr = new int[n + 1];
     for (int i = 0; i < n; i++) {
         cin >> A[i];
         if (A[i] > 1000) {
             cerr << i << " > 1000";
+            delete[] A;
+            delete[] B;
+            delete[] pr;
             return 0;
         }
     }
+    if (n == 3) {
+        cout << A[2] + A[0] << endl;
+        cout << "1 3";
+        delete[] A;
+        delete[] B;
+        delete[] pr;
+        return 0;
+    }
     if (n == 2) {
         cout << "-1";
+        delete[] A;
+        delete[] B;
+        delete[] pr;
         return 0;
     }
     if (n == 1) {
-        cout << A[0];
+        cout << A[0] << endl;
         cout << "1";
+        delete[] A;
+        delete[] B;
+        delete[] pr;
         return 0;
     }
-
-    int k = n;
-    int* B = new int[k];
-    int j = 1;
-    B[0] = 0;
-
-    // Основной цикл
-    for (int i = 2; i < n; i++) {
-        if (i > 4) {
+    pr[0] = -1;
+    pr[1] = -1;
+    pr[2] = 1;
+    pr[3] = 1;
+    pr[4] = 3;
+    A[1] = -1;
+    A[2] = A[2] + A[0];
+    
+    for (int i = 3; i < n; i++) {
             if ((A[i] + A[i - 3]) < (A[i] + A[i - 2])) {
                 A[i] = A[i] + A[i - 2];
-                B[j] = i - 1;
-               /* B[l] = or2;
-                l++;*/
-                
+                pr[i] = i - 1;
+                j++;
             }
             else {
                 A[i] = A[i] + A[i - 3];
-                B[j] = i - 2;
-               /* B[l] = or1;
-                l++;*/
+                pr[i] = i - 2;
+                j++;
             }
-        }
-        else {
-            if (i == 2 || i == 3) {
-                A[i] = A[i] + A[0];
-                
-                
-            }
-            if (i == 4) {
-                A[4] = A[4] + A[2];
-               
-            }
-        }
-        j++;
+    }
 
-    }
+    cout << A[n - 1] << endl;
+    pr[n] = n;
     
-    cout << A[n-1]<<endl;
-    /*B[0] = n;
-    for (int i = n-1; i >= 0; i--) {
-        int k = i;
-        if (A[i - 2] < A[i - 3]) {
-            k = k - 3;
-            B[j] = i - 2;
+
+    B[0] = n;
+    int k = 1;
+    for (int i = n - 1; i > 0; i--) {
+        while (pr[i] != -1) {
+            B[k] = pr[i];
+            i = pr[i] - 1;
+            k++;
+
         }
-        if (A[i - 2] > A[i - 3]) {
-            k = k - 2;
-            B[j] = i - 1;
-        }
-        if (A[i - 2] == A[i - 3]) {
-            k = k - 2;
-            B[j] = i - 1;
-        }
-        j++;
-        i = k;
     }
-    B[j] = 1;*/
-    for (int i = 0; i <= j; i++) {
+
+    for (int i = k - 1; i >= 0; i--) {
         cout << B[i] << " ";
     }
-    
-
+    delete[] A;
+    delete[] B;
+    delete[] pr;
+    return 0;
 }
